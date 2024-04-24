3bc45cdd4b16f3b954c4bfc06e1dd2230df8905d
hzhex <notifono@ya.ru>
2024-04-24 18:08:16 +0500

    var5

--- 4.50true/MyForm.h
+++ 4.50true/MyForm.h
@@ -12,17 +12,19 @@ namespace My450 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace msclr::interop;
 	using namespace std;
 
-	struct goods
+	struct person
 	{
-		string name;
-		string country;
-		int importAmount;
+		string fullName;
+		int day;
+		string month;
+		int year;
 
 	};
 
-	vector<goods> goodsData;
+	vector<person> personsData;
 
 	/// <summary>
 	/// Summary for MyForm
@@ -50,19 +52,39 @@ namespace My450 {
 			}
 		}
 	private: System::Windows::Forms::DataGridView^ dataGridView1;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column1;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column2;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column3;
+
+
+
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::TextBox^ textBox2;
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::TextBox^ textBox3;
 	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::Label^ label4;
-	private: System::Windows::Forms::TextBox^ textBox4;
+
+
+
+
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column2;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column3;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column4;
+	private: System::Windows::Forms::TextBox^ textBox4;
+
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::DataGridView^ dataGridView2;
+
+
+
+
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column5;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column6;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column7;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column8;
 	protected:
 
 	protected:
@@ -99,64 +121,79 @@ namespace My450 {
 			this->Column1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Column2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Column3 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column4 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
 			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->dataGridView2 = (gcnew System::Windows::Forms::DataGridView());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->Column5 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column6 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column7 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column8 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// dataGridView1
 			// 
 			this->dataGridView1->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::Fill;
 			this->dataGridView1->AutoSizeRowsMode = System::Windows::Forms::DataGridViewAutoSizeRowsMode::AllCellsExceptHeaders;
+			this->dataGridView1->BackgroundColor = System::Drawing::Color::RosyBrown;
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(3) {
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(4) {
 				this->Column1,
-					this->Column2, this->Column3
+					this->Column2, this->Column3, this->Column4
 			});
-			this->dataGridView1->Location = System::Drawing::Point(12, 12);
+			this->dataGridView1->Location = System::Drawing::Point(12, 33);
 			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(437, 214);
+			this->dataGridView1->Size = System::Drawing::Size(548, 243);
 			this->dataGridView1->TabIndex = 0;
 			// 
 			// Column1
 			// 
-			this->Column1->HeaderText = L"Товар";
+			this->Column1->HeaderText = L"ФИО";
 			this->Column1->Name = L"Column1";
 			// 
 			// Column2
 			// 
-			this->Column2->HeaderText = L"Страна";
+			this->Column2->HeaderText = L"Дата роджения";
 			this->Column2->Name = L"Column2";
 			// 
 			// Column3
 			// 
-			this->Column3->HeaderText = L"Объем поставок";
+			this->Column3->HeaderText = L"Месяц рождения";
 			this->Column3->Name = L"Column3";
+			// 
+			// Column4
+			// 
+			this->Column4->HeaderText = L"Год рождения";
+			this->Column4->Name = L"Column4";
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(481, 13);
+			this->label1->Location = System::Drawing::Point(583, 33);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(184, 20);
+			this->label1->Size = System::Drawing::Size(116, 20);
 			this->label1->TabIndex = 1;
-			this->label1->Text = L"Наименование товара:";
+			this->label1->Text = L"Фамилия И.О.";
 			// 
 			// textBox1
 			// 
 			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->textBox1->Location = System::Drawing::Point(485, 36);
+			this->textBox1->Location = System::Drawing::Point(587, 56);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(278, 26);
 			this->textBox1->TabIndex = 2;
@@ -165,7 +202,7 @@ namespace My450 {
 			// 
 			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->textBox2->Location = System::Drawing::Point(485, 100);
+			this->textBox2->Location = System::Drawing::Point(587, 120);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(278, 26);
 			this->textBox2->TabIndex = 4;
@@ -175,17 +212,17 @@ namespace My450 {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(481, 77);
+			this->label2->Location = System::Drawing::Point(583, 97);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(138, 20);
+			this->label2->Size = System::Drawing::Size(127, 20);
 			this->label2->TabIndex = 3;
-			this->label2->Text = L"Страна импорта:";
+			this->label2->Text = L"Дата рожления";
 			// 
 			// textBox3
 			// 
 			this->textBox3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->textBox3->Location = System::Drawing::Point(485, 167);
+			this->textBox3->Location = System::Drawing::Point(587, 187);
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->Size = System::Drawing::Size(278, 26);
 			this->textBox3->TabIndex = 6;
@@ -195,60 +232,122 @@ namespace My450 {
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label3->Location = System::Drawing::Point(481, 144);
+			this->label3->Location = System::Drawing::Point(583, 164);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(139, 20);
+			this->label3->Size = System::Drawing::Size(137, 20);
 			this->label3->TabIndex = 5;
-			this->label3->Text = L"Объем поставок:";
-			// 
-			// label4
-			// 
-			this->label4->AutoSize = true;
-			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label4->Location = System::Drawing::Point(481, 304);
-			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(524, 20);
-			this->label4->TabIndex = 7;
-			this->label4->Text = L"Введите название товара, по которому вы хотите вывести данные:";
-			// 
-			// textBox4
-			// 
-			this->textBox4->Location = System::Drawing::Point(485, 346);
-			this->textBox4->Name = L"textBox4";
-			this->textBox4->Size = System::Drawing::Size(278, 20);
-			this->textBox4->TabIndex = 8;
+			this->label3->Text = L"Месяц рождения";
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(485, 215);
+			this->button1->Location = System::Drawing::Point(12, 300);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(135, 46);
+			this->button1->Size = System::Drawing::Size(548, 48);
 			this->button1->TabIndex = 9;
-			this->button1->Text = L"Внести данные";
+			this->button1->Text = L"Внести данные в таблицу";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			this->button1->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::button1_MouseClick);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(485, 387);
+			this->button2->Location = System::Drawing::Point(587, 441);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(135, 46);
+			this->button2->Size = System::Drawing::Size(194, 159);
 			this->button2->TabIndex = 10;
-			this->button2->Text = L"Вывести данные";
+			this->button2->Text = L"Вывести список друзей, которым в этом году исполняется 25 лет";
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// textBox4
+			// 
+			this->textBox4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox4->Location = System::Drawing::Point(587, 254);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(278, 26);
+			this->textBox4->TabIndex = 12;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label5->Location = System::Drawing::Point(583, 231);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(118, 20);
+			this->label5->TabIndex = 11;
+			this->label5->Text = L"Год рождения";
+			// 
+			// dataGridView2
+			// 
+			this->dataGridView2->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::Fill;
+			this->dataGridView2->AutoSizeRowsMode = System::Windows::Forms::DataGridViewAutoSizeRowsMode::AllCellsExceptHeaders;
+			this->dataGridView2->BackgroundColor = System::Drawing::SystemColors::InactiveCaption;
+			this->dataGridView2->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView2->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(4) {
+				this->Column5,
+					this->Column6, this->Column7, this->Column8
+			});
+			this->dataGridView2->Location = System::Drawing::Point(17, 398);
+			this->dataGridView2->Name = L"dataGridView2";
+			this->dataGridView2->Size = System::Drawing::Size(548, 243);
+			this->dataGridView2->TabIndex = 13;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(12, 9);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(196, 20);
+			this->label4->TabIndex = 14;
+			this->label4->Text = L"Записная книга/таблица";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label6->Location = System::Drawing::Point(13, 369);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(259, 20);
+			this->label6->TabIndex = 15;
+			this->label6->Text = L"Список тех, кому исполняется 25";
+			// 
+			// Column5
+			// 
+			this->Column5->HeaderText = L"ФИО";
+			this->Column5->Name = L"Column5";
+			// 
+			// Column6
+			// 
+			this->Column6->HeaderText = L"Дата роджения";
+			this->Column6->Name = L"Column6";
+			// 
+			// Column7
+			// 
+			this->Column7->HeaderText = L"Месяц рождения";
+			this->Column7->Name = L"Column7";
+			// 
+			// Column8
+			// 
+			this->Column8->HeaderText = L"Год рождения";
+			this->Column8->Name = L"Column8";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1017, 615);
+			this->ClientSize = System::Drawing::Size(1017, 653);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->dataGridView2);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->label5);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
-			this->Controls->Add(this->textBox4);
-			this->Controls->Add(this->label4);
 			this->Controls->Add(this->textBox3);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->textBox2);
@@ -259,6 +358,7 @@ namespace My450 {
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -266,59 +366,51 @@ namespace My450 {
 #pragma endregion
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 
-		String^ name = textBox1->Text;
-		String^ country = textBox2->Text;
-		String^ amount = textBox3->Text;
+		String^ fullNameT = textBox1->Text;
+		String^ dayT = textBox2->Text;
+		String^ monthT = textBox3->Text;
+		String^ yearT = textBox4->Text;
 
 		int rowIndex = dataGridView1->Rows->Add();
 
-		dataGridView1->Rows[rowIndex]->Cells["Column1"]->Value = name;
-		dataGridView1->Rows[rowIndex]->Cells["Column2"]->Value = country;
-		dataGridView1->Rows[rowIndex]->Cells["Column3"]->Value = amount;
+		dataGridView1->Rows[rowIndex]->Cells["Column1"]->Value = fullNameT;
+		dataGridView1->Rows[rowIndex]->Cells["Column2"]->Value = dayT;
+		dataGridView1->Rows[rowIndex]->Cells["Column3"]->Value = monthT;
+		dataGridView1->Rows[rowIndex]->Cells["Column4"]->Value = yearT;
 	}
 
 	private: System::Void button1_MouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
 
-		String^ nameStr = textBox1->Text;
-		String^ countryStr = textBox2->Text;
-		String^ importAmountStr = textBox3->Text;
+		string fullName = marshal_as<string>(textBox1->Text);
+		int day = Convert::ToInt32(textBox2->Text);
+		string month = marshal_as<string>(textBox3->Text);
+		int year = Convert::ToInt32(textBox4->Text);
 
-		string name = msclr::interop::marshal_as<std::string>(nameStr);
-		string country = msclr::interop::marshal_as<std::string>(countryStr);
-		int importAmount = Convert::ToInt32(importAmountStr);
+		person newPersons;
+		newPersons.fullName = fullName;
+		newPersons.day = day;
+		newPersons.month = month;
+		newPersons.year = year;
 
-		goods newGoods;
-		newGoods.name = name;
-		newGoods.country = country;
-		newGoods.importAmount = importAmount;
-
-		goodsData.push_back(newGoods);
+		personsData.push_back(newPersons);
 	}
 	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-		String^ requestedName = textBox4->Text;
-		String^ exportedCountries = "";
-		String^ exportedAmounts = "";
 
-		for (const auto& goodsItem : goodsData) {
-			if (requestedName == gcnew String(goodsItem.name.c_str())) {
-				exportedCountries += gcnew String(goodsItem.country.c_str()) + " ";
-				exportedAmounts += goodsItem.importAmount.ToString() + " ";
-			}
+		for each (person personUnique in personsData) {
+			if (personUnique.year == 1999) {
+				String^ fullNameT = gcnew String(personUnique.fullName.c_str());
+				String^ dayT = gcnew String(to_string(personUnique.day).c_str());
+				String^ monthT = gcnew String(personUnique.month.c_str());
+				String^ yearT = gcnew String(to_string(personUnique.year).c_str());
+
+				int rowIndex = dataGridView2->Rows->Add();
+
+				dataGridView2->Rows[rowIndex]->Cells["Column5"]->Value = fullNameT;
+				dataGridView2->Rows[rowIndex]->Cells["Column6"]->Value = dayT;
+				dataGridView2->Rows[rowIndex]->Cells["Column7"]->Value = monthT;
+				dataGridView2->Rows[rowIndex]->Cells["Column8"]->Value = yearT;
+			};
 		}
-
-		for (const auto& goodsItem : goodsData) {
-			if (requestedName == gcnew String(goodsItem.name.c_str())) {
-				String^ infoMessage = "Товар: " + gcnew String(goodsItem.name.c_str()) + "\n";
-				infoMessage += "Экспортируется в: " + exportedCountries + "\n";
-				infoMessage += "Объем экспорта (соответственно к каждой стране): " + exportedAmounts;
-
-				MessageBox::Show(infoMessage, "Информация о товаре", MessageBoxButtons::OK, MessageBoxIcon::Information);
-				return; // Exit the loop once the goods are found
-			}
-		}
-
-		// If goods not found, display an error message
-		MessageBox::Show("Товар не найден!", "Ошибка", MessageBoxButtons::OK, MessageBoxIcon::Error);
 }
 };
 }
